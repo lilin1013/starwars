dc7e7a7a2a00fef22503dd1e5a988ebd2b4a010c
lilin <user@lilins-MacBook-Pro.local>
2017-02-16 21:44:43 +0100

    adding library

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var starWarsNames = require('./starWarsNames.json');
+var uniqueRandomArray = require('unique-random-array');
+module.exports = {
+  all: starWarsNames,
+  random: uniqueRandomArray(starWarsNames)
+};
